0ff14fed437ea39000ac6114620210f46a25e83d
Kathiresh K <kathireshkthr@gmail.com>
2026-02-26 09:53:52 +0530

    New Files

--- Python.py
+++ Python.py
@@ -1,1 +1,21 @@
-print("Hello World")
+#1.Getter and Setter
+
+class vechicle:
+    def __init__(self,brand,wheel,mileage):
+        self.brand = brand
+        self.__wheel = wheel
+        self.mileage = mileage
+
+    def get_no_of_wheel(self):
+        return self.__wheel
+
+    def set_no_of_wheel(self,wheel):
+        self.__wheel = wheel
+
+car1 = vechicle("Victara",4,25)
+
+print("No of Wheel : ",car1.get_no_of_wheel())
+
+car1.set_no_of_wheel(6)
+
+print("After set no.of Wheel is : ",car1.get_no_of_wheel())
